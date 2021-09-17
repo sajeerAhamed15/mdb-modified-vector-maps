e0bd3107798648008f3786273bab5e443b72fd78
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2021-09-17 14:49:17 +0200

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+declare const MDBVectorMap: React.FunctionComponent<{
+  className?: string,
+  hoverFill?: string,
+  selectRegion?: string,
+  readonly?: boolean,
+  selectFill?: string,
+  zoomEvents?: boolean,
+  hover?: boolean,
+  fill?: string,
+  fillOpacity?: number,
+  stroke?: string,
+  strokeOpacity?: number,
+  strokeLinejoin?: string,
+  tooltips?: boolean,
+  markerFill?: string,
+  markerStroke?: string,
+  markerInnerFill?: string,
+  btnClass?: string,
+  [rest: string]: any;
+}>;
+
+export { MDBVectorMap };
