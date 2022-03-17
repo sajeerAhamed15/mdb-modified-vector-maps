a35d86848f88bb2fb872369a597260c87056e54d
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2022-03-17 11:48:48 +0100

    add 3.0.0 production build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,24 +1,36 @@
-import * as React from 'react';
+import React$1 from 'react';
 
-declare const MDBVectorMap: React.FunctionComponent<{
-  className?: string,
-  hoverFill?: string,
-  selectRegion?: string,
-  readonly?: boolean,
-  selectFill?: string,
-  zoomEvents?: boolean,
-  hover?: boolean,
-  fill?: string,
-  fillOpacity?: number,
-  stroke?: string,
-  strokeOpacity?: number,
-  strokeLinejoin?: string,
-  tooltips?: boolean,
-  markerFill?: string,
-  markerStroke?: string,
-  markerInnerFill?: string,
-  btnClass?: string,
-  [rest: string]: any;
-}>;
+interface VectorMapProps extends Omit<React$1.AllHTMLAttributes<HTMLDivElement>, 'height' | 'width'> {
+    hoverFill?: string;
+    selectRegion?: string;
+    readonly?: boolean;
+    selectFill?: string;
+    zoomEvents?: boolean;
+    hover?: boolean;
+    fill?: string;
+    fillOpacity?: number;
+    stroke?: string;
+    strokeOpacity?: number;
+    strokeLinejoin?: string;
+    tooltips?: boolean;
+    markerFill?: string;
+    markerStroke?: string;
+    markerInnerFill?: string;
+    btnClass?: string;
+    map?: string | React$1.ComponentProps<any>;
+    height?: number | null;
+    width?: number | null;
+    strokeWidth?: number;
+    zoomMin?: number | null;
+    zoomMax?: number | null;
+    zoomStep?: number | null;
+    scale?: number;
+    markers?: Record<string, any>;
+    markerStrokeWidth?: number;
+    mapRef?: React$1.MutableRefObject<any>;
+    colorMap?: Record<string, any>;
+}
+
+declare const MDBVectorMap: React.FunctionComponent<VectorMapProps>;
 
 export { MDBVectorMap };
